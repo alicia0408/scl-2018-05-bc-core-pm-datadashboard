74549e864f9e7003924616d9a78d7741d6576bbc
milelym <milelymarcial@gmail.com>
2018-06-16 17:06:10 -0400

    solucion de conflicto en cohorts cambios para traer data

--- src/data.js
+++ src/data.js
@@ -1,11 +1,49 @@
-function handleSuccess(){
-    console.log(this.responseText);
+let data={} ;
+function loadusers() {
+     fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+        .then(function(resp){
+            return resp.json();
+            //retorna la data
+        })
+        // maneja la data
+        .then(function(valores){
+            data.user=valores;
+            console.log('data.user es :',data.user);
+            loadcortes(); 
+        });
+ }
 
+ function loadcortes() {
+    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json')
+       .then(function(resp){
+           return resp.json();
+           //retorna la data
+       })
+       // maneja la data
+       .then(function(valores){
+           data.cortes=valores;
+           console.log('data.cortes es :',data.cortes);
+           loadprocess();
+       });
 }
-function handleError(){
-    console.log('ah error');
+
+function loadprocess() {
+    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+       .then(function(resp){
+           return resp.json();
+           //retorna la data
+       })
+       // maneja la data
+       .then(function(valores){
+           data.procesos=valores;
+           console.log('data.procesos es :',data.procesos);
+           final_load();
+       });
 }
-const asyncRequestObject = new XMLHttpRequest();
-asyncRequestObject.open('GET','https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json'); //URL de usuarios de un cohort de lima
-asyncRequestObject.onerror=handleError;
-asyncRequestObject.send();
+
+function final_load()
+{
+    console.log('la data final:',data)
+}
+loadusers();
+ console.log('esta hoja');
