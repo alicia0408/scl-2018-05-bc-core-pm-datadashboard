24fe34a296ee5e103b440ace9ab40cbe59d4ada5
Alicia Centeno <alicent.08.02@gmail.com>
2018-07-01 14:22:49 -0400

    se cambia fetch a main.js

--- src/data.js
+++ src/data.js
@@ -1,47 +1,4 @@
-let data = {};/* con esta variable luego se llamara en la carpeta main*/
-function loadusers() {
-    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json')
-        .then(function(resp) {
-            return resp.json();
-            //retorna la data
-        })
-        // maneja la data se crea una variable fuera de la funcion y los valores se guardan en un objeto//
-        .then(function (valores) {
-            data.user = valores;
-           // console.log('data.user es :',data.user);
-            // para imprimirlo cuando se obtenda la informacion//
-            loadcortes();
-        });
-}
-// para que no se carguen asincronamente se crea la funcion dentro del fetch 
-function loadcortes() {
-    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json')
-        .then(function (resp) {
-            return resp.json();
-            // retorna la data
-        })
-        // maneja la data
-        .then(function (valores) {
-            data.cortes = valores;
-            // console.log('data.cortes es :',data.cortes);
-            imprCort();
-            loadprogress();
-        });
-}
-function loadprogress() {
-    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
-        .then(function (resp) {
-            return resp.json();
-            // retorna la data
-        })
-        // maneja la data
-        .then(function (valores) {
-            data.progress = valores;
-         //   console.log('data.progress es :', data.progress);
-            imprProgress();
-            imprUser(computeUsersStats(data.user, data.progress, data.cortes));
-        });
-}
+
 function computeUsersStats (user, progress, courses)// todos los usrs ordenados
 {
     for (let i = 0; i < user.length; i++) {
@@ -144,3 +101,37 @@ function processCohortData({ cohortData, orderBy, orderDirection, filterBy }) {
     return datos;
 }
 loadusers();
+function imprCort() {
+    const cohortesLim = data.cortes.filter(cohorte => {
+        return cohorte.id.indexOf('lim') >= 0;
+    });
+    for (i = 0; i < cohortesLim.length; i++) {
+        // console.log(data.cortes[i].id);
+        let DataCortes = (cohortesLim[i].id);
+        //console.log(DataCortes);
+        // hacer un evento click y 
+    }
+}
+function filterData(search) {
+    const cohortes = data.cortes.filter(cohorte => {
+        return cohorte.id.indexOf(search) >= 0;
+    });
+    /* for (i = 0; i < cohortes.length; i++) {
+         // console.log(data.cortes[i].id);
+         let DataCortes = (cohortes[i].id);
+        
+       // hacer un evento click y 
+     }
+     console.log(search+':',cohortes);*/
+
+}
+function imprProgress() {
+    for (let key in data.progress) {// el for recorre el progress que es un objeto de objetos y key da acceso al segundo obj//
+        // console.log(key,data.progress[key]);// llave y su valor como referencia //
+        if (data.progress[key].intro) {
+            // console.log(data.progress[key].intro.percent);  
+        } else {
+            // console.log('no esxite:',key);
+        }
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
+let data = {};/* con esta variable luego se llamara en la carpeta main*/
+function loadusers() {
+    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+        .then(function (resp) {
+            return resp.json();
+            //retorna la data
+        })
+        // maneja la data se crea una variable fuera de la funcion y los valores se guardan en un objeto//
+        .then(function (valores) {
+            data.user = valores;
+            // console.log('data.user es :',data.user);
+            // para imprimirlo cuando se obtenda la informacion//
+            loadcortes();
+        });
+}
+// para que no se carguen asincronamente se crea la funcion dentro del fetch 
+function loadcortes() {
+    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json')
+        .then(function (resp) {
+            return resp.json();
+            // retorna la data
+        })
+        // maneja la data
+        .then(function (valores) {
+            data.cortes = valores;
+            // console.log('data.cortes es :',data.cortes);
+            imprCort();
+            loadprogress();
+        });
+}
+function loadprogress() {
+    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+        .then(function (resp) {
+            return resp.json();
+            // retorna la data
+        })
+        // maneja la data
+        .then(function (valores) {
+            data.progress = valores;
+            //   console.log('data.progress es :', data.progress);
+            imprProgress();
+            imprUser(computeUsersStats(data.user, data.progress, data.cortes));
+        });
+}
+
+
+
+
 function imprUser(user) {
     var datos = document.querySelector('#datos');
     datos.innerHTML = '';
