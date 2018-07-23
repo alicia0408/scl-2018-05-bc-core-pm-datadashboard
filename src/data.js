6962015ac3eebcebea29d60e5a392a21c1d4c326
Alicia Centeno <alicent.08.02@gmail.com>
2018-07-23 15:13:13 -0400

    se invoca data por medio de promesse.all

--- src/data.js
+++ src/data.js
@@ -1,85 +1,5 @@
-window.computeUsersStats = (user, progress, courses) => {// todos los usrs ordenados
 
-    for (let i = 0; i < user.length; i++) {
-        user[i].stats={};
-            for (var key in progress) {
-                    if (key == user[i].id){
-                        if (progress[key].intro) {
-                            // console.log(progress[key]);
-                                    let percent = 0
-                                    if (progress[key].intro.percent)
-                                        percent = progress[key].intro.percent;
-                                    let llaves = getformat(progress[key].intro);
-                                    user[i].stats = { percent: percent, exercises: llaves.exercises, reads: llaves.reads, quizzes: llaves.quizzes };
-                        }
-                        else{
-                            var cursos = { exercises: {}, reads: {}, quizzes: {} };
-                            cursos.exercises = { completed: 0, total: 0, percent: 0 };
-                            cursos.reads = { completed: 0, total: 0, percent: 0 };
-                            cursos.quizzes = { completed: 0, total: 0, percent: 0, scoreSum: 0, scoreAvg: 0 };
-                            user[i].stats = cursos;
-                        }
-                    }
-                
-            }
-           
-    }
-    // filterData('lim');
-    // filterData('scl');
-  // console.log('data Unida :', user);
-    return user;
-}
-function getformat(intro) {
-    cursos = { exercises: {}, reads: {}, quizzes: {} };
-    cursos.exercises = { completed: 0, total: 0, percent: 0 };
-    cursos.reads = { completed: 0, total: 0, percent: 0 };
-    cursos.quizzes = { completed: 0, total: 0, percent: 0, scoreSum: 0, scoreAvg: 0 };
-    for (var key in intro.units) {
-        if (intro.units[key].parts) {
-            for (var keyparts in intro.units[key].parts) {
-                if (intro.units[key].parts[keyparts].type == 'practice') {
-                    if (intro.units[key].parts[keyparts].completed == 1)
-                        cursos.exercises.completed++;
-                    cursos.exercises.total++;
-                }
-                if (intro.units[key].parts[keyparts].type == 'read') {
-                    if (intro.units[key].parts[keyparts].completed == 1)
-                        cursos.reads.completed++;
-                    cursos.reads.total++;
-                }
-                if (intro.units[key].parts[keyparts].type == 'quiz') {
-                    if (intro.units[key].parts[keyparts].completed == 1) {
-                        cursos.quizzes.completed++;
-                        if (intro.units[key].parts[keyparts].score)
-                            cursos.quizzes.scoreSum += intro.units[key].parts[keyparts].score;
-                    }
-
-                    cursos.quizzes.total++;
-
-                }
-            }
-
-        }
-    }
-    if (cursos.exercises.total > 0) {
-        cursos.exercises.percent = (cursos.exercises.completed / cursos.exercises.total) * 100;
-        cursos.exercises.percent = parseFloat(cursos.exercises.percent.toFixed(2));
-    }
-    if (cursos.reads.total > 0) {
-        cursos.reads.percent = (cursos.reads.completed / cursos.reads.total) * 100;
-        cursos.reads.percent = parseFloat(cursos.reads.percent.toFixed(2));
-    }
-    if (cursos.quizzes.total > 0) {
-        cursos.quizzes.percent = (cursos.quizzes.completed / cursos.quizzes.total) * 100;
-        cursos.quizzes.percent = parseFloat(cursos.quizzes.percent.toFixed(2));
-
-        cursos.quizzes.scoreAvg = (cursos.quizzes.scoreSum / cursos.quizzes.completed);
-        cursos.quizzes.scoreAvg = parseFloat(cursos.quizzes.scoreAvg.toFixed(2));
-    }
-    return cursos;
-}
-
-window.sortUsers = (users, orderBy, orderDirection) => {
+/*window.sortUsers = (users, orderBy, orderDirection) => {
     var datos = [];
     
     if(orderBy === 'Nombre'){
@@ -157,20 +77,14 @@ window.sortUsers = (users, orderBy, orderDirection) => {
                         datos = datos.reverse();
                     }
     return datos;
-}
+}*/
 
-function filterUsers(users, filterBy) {
+/*function filterUsers(users, filterBy) {
     var users = users.name;
 users.sort(function (a, b) {
   return a.localeCompare(b);
   
 });
 console.log(filterUsers);
-}
+}*/
 
-function processCohortData({ cohortData, orderBy, orderDirection, filterBy }) {
-    var datos = [];
-
-    return datos;
-}
-loadusers();
--- src/main.js
+++ src/main.js
@@ -1,111 +1,23 @@
-let data = {};/* con esta variable luego se llamara en la carpeta main*/
-function loadusers() {
-    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json')
-        .then(function (resp) {
-            return resp.json();
-            //retorna la data
-        })
-        // maneja la data se crea una variable fuera de la funcion y los valores se guardan en un objeto//
-        .then(function (valores) {
-            data.user = valores;
-            // console.log('data.user es :',data.user);
-            // para imprimirlo cuando se obtenda la informacion//
-            loadcortes();
-        });
-}
-// para que no se carguen asincronamente se crea la funcion dentro del fetch 
-function loadcortes() {
-    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json')
-        .then(function (resp) {
-            return resp.json();
-            // retorna la data
-        })
-        // maneja la data
-        .then(function (valores) {
-            data.cortes = valores;
-            // console.log('data.cortes es :',data.cortes);
-            imprCort();
-            loadprogress();
-        });
-}
-function loadprogress() {
-    fetch('https://milelym.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
-        .then(function (resp) {
-            return resp.json();
-            // retorna la data
-        })
-        // maneja la data
-        .then(function (valores) {
-            data.progress = valores;
-            //   console.log('data.progress es :', data.progress);
-            imprProgress();
-            imprUser(computeUsersStats(data.user, data.progress, data.cortes));
-        });
-}
-
-
-
-
-function imprUser(user) {
-    var datos = document.querySelector('#datos');
-    datos.innerHTML = '';
-    
-    for(i = 0; i < user.length; i++) {
-        // console.log(data.user[i])
-        let TodoUser = (user[i]);
-        
-        datos.innerHTML +=`
-            <tr class="row col-md-12 t-a-c" >
-                <th class="col-md-2 ln-correct-50">${TodoUser.name}</th>
-                <td class="col-md-1 ln-correct-50">${TodoUser.stats.percent + " % "}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.exercises.total}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.exercises.completed}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.exercises.percent + " % "}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.reads.total}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.reads.completed}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.reads.percent + " % "}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.quizzes.total}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.quizzes.completed}</td>
-                <td class="col-md-1 secundary">${TodoUser.stats.quizzes.percent + " % "}</td>
-
-            </tr>
-         `;
-    }
-    document.querySelector("#loader").style.display="none";
-   
-}
-
-function imprCort() {
-    const cohortesLim = data.cortes.filter(cohorte => {
-        return cohorte.id.indexOf('lim') >= 0;
-    });
-    for (i = 0; i < cohortesLim.length; i++) {
-        // console.log(data.cortes[i].id);
-        let DataCortes = (cohortesLim[i].id);
-        //console.log(DataCortes);
-        // hacer un evento click y 
-    }
-}
-function filterData(search) {
-    const cohortes = data.cortes.filter(cohorte => {
-        return cohorte.id.indexOf(search) >= 0;
-    });
-    /* for (i = 0; i < cohortes.length; i++) {
-         // console.log(data.cortes[i].id);
-         let DataCortes = (cohortes[i].id);
-        
-       // hacer un evento click y 
-     }
-     console.log(search+':',cohortes);*/
+Promise.all([
+    fetch("../data/cohorts/lim-2018-03-pre-core-pw/users.json"),
+    fetch("../data/cohorts/lim-2018-03-pre-core-pw/progress.json"),
+    fetch("../data/cohorts.json")
+]).then((Responses) => { // responde a todas las promesas
+    return Promise.all(Responses.map((response) => {
+        return response.json();
+    }));
 
 }
-function imprProgress() {
-    for (let key in data.progress) {// el for recorre el progress que es un objeto de objetos y key da acceso al segundo obj//
-        // console.log(key,data.progress[key]);// llave y su valor como referencia //
-        if (data.progress[key].intro) {
-            // console.log(data.progress[key].intro.percent);  
-        } else {
-            // console.log('no esxite:',key);
-        }
-    }
+).then((responseJsons) => {// arreglo de respuestas json
+    console.log(responseJsons);
+
+    /*let users = response[0];
+    let progress = response[1];
+    let courses = response[2];
+    let computeUsersStats = window.loadData.computeUsersStats(users, progress, Object.keys(cohorts[0].coursesIndex));*/
+
+}).catch((error) => {
+    alert("Error de carga" + error);
+    //console.log(error)
 }
+);
